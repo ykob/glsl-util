13c35a57b058beec9ff9b02bcb28765ce4e27716
yoichi kobayashi <info@tplh.net>
2018-01-11 17:16:47 +0900

    renamed polar function to spherical.

--- polar.glsl
+++ polar.glsl
@@ -1,8 +1,8 @@
-vec3 polar(float radian1, float radian2, float radius) {
+vec3 spherical(float radian1, float radian2, float radius) {
   return vec3(
     cos(radian1) * cos(radian2) * radius,
     sin(radian1) * radius,
     cos(radian1) * sin(radian2) * radius
   );
 }
-#pragma glslify: export(polar)
+#pragma glslify: export(spherical)
